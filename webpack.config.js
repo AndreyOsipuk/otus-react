98e24451f0fee9c9e90ecbe87daad5c71eef3dcf
AndreyOsipuk <andrey.o@list.ru>
2020-04-23 18:37:09 +0400

    build

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         filename: "index.[chunkhash].js",
     },
     resolve: {
-        extensions: ['.tsx','.jsx', '.js'],
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
         alias: {
             assets: path.resolve(__dirname, 'src', 'assets'),
             components: path.resolve(__dirname, 'src', 'components'),
